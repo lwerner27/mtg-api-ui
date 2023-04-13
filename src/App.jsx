dd9d541abddef2d702fbde030da50fea68abf6bf
Lucas Werner <lwerner27@gmail.com>
2023-04-13 18:25:05 -0500

    Initial project setup

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+function App() {
+    return (
+        <div className='App'>
+            <div className='container mx-auto'>
+                <h1>Hello World</h1>
+            </div>
+        </div>
+    );
+}
+
+export default App;
