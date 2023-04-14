69c56e38d8ddb4adc2d31d82407c27da0808e771
Lucas Werner <lwerner27@gmail.com>
2023-04-13 20:08:58 -0500

    Added a home page component

---
+++ src/Pages/Home/index.jsx
@@ -0,0 +1,9 @@
+function Home() {
+    return (
+        <div className='container mx-auto flex justify-center'>
+            <h1>Welcome to the MTG API Explorer</h1>
+        </div>
+    );
+}
+
+export default Home;
