e52bc9a1694b7f52fe987116a0532ecfdb9f6af8
Lucas Werner <lwerner27@gmail.com>
2023-04-13 20:47:56 -0500

    Started basic setup of the card search and datalist

--- src/Pages/Home/index.jsx
+++ src/Pages/Home/index.jsx
@@ -1,7 +1,22 @@
+import CustomTextInput from '../../Components/CustomTextInput';
 function Home() {
     return (
-        <div className='container mx-auto flex justify-center'>
-            <h1>Welcome to the MTG API Explorer</h1>
+        <div className='container mx-auto flex flex-col items-center'>
+            <h1 className='text-4xl text-center'>
+                Welcome to the MTG API Explorer
+            </h1>
+            <CustomTextInput
+                inputType='Text'
+                inputId='card-search'
+                inputPlaceholder='Zur the Enchanter'
+                additionalClasses={'mt-5 md:w-full lg:w-1/2'}
+                dataList='cardNames'
+            />
+            <datalist id='cardNames'>
+                <option value='Zur the Enchanter' />
+                <option value='Opt' />
+                <option value='Pacifism' />
+            </datalist>
         </div>
     );
 }
