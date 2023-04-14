3fdd5f462b1da8b6158e6c5ed10b97821f1c34ab
Lucas Werner <lwerner27@gmail.com>
2023-04-13 20:46:52 -0500

    Created custom input field component

---
+++ src/Components/CustomTextInput/index.jsx
@@ -0,0 +1,24 @@
+function CustomTextInput({
+    inputId,
+    inputType,
+    inputPlaceholder,
+    dataList,
+    additionalClasses,
+}) {
+    const handleChange = (event) => {
+        // console.log(event.target.value);
+    };
+
+    return (
+        <input
+            type={inputType}
+            id={inputId}
+            placeholder={inputPlaceholder}
+            onChange={handleChange}
+            list={dataList}
+            className={`rounded-lg ${additionalClasses}`}
+        />
+    );
+}
+
+export default CustomTextInput;
