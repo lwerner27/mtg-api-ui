6e3566f5a06e532227c02a32ae43cb84cff058d4
Lucas Werner <lwerner27@gmail.com>
2023-04-17 21:07:20 -0500

    simplified the getCardnames function

--- src/Helpers/getCardNames.js
+++ src/Helpers/getCardNames.js
@@ -1,21 +1,9 @@
-function getCardNames(searchTerm, cardNames) {
-    let cardNames = cardNames 
-    
-    // First Determine if we need to get cardNames.
-    // Only get card names if search terms is greater thatn 3 and cardNames === null
-    if (searchTerm.length > 2 && cardNames === null) {
-        // Make a request to the database for card unique card names based on the current searchTerm 
-        // cardNames = await DB request
-    }
-
-    // If we clear our search term clear our list of names 
-    if (searchTerm.length < 3 && cardNames != null) {
-        // set cardNames to null
-    }
-
-    // Return updated cardNames
-    return cardNames
-
+async function getCardNames(requestURL, setCardNames) {
+    // Get all the unique card names
+    const res = await fetch(requestURL)
+    const cardNames = await res.json()
+    console.log(cardNames)
+    setCardNames(cardNames)
 }
 
 export default getCardNames
